35f77b1b14b04ce45784914091ee490dc49c6ca7
masudcodex <forester.masud@gmail.com>
2022-09-28 22:52:30 +0600

    fontawesome installed and added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
+
+const Header = () => {
+    return (
+    <div>
+        <FontAwesomeIcon icon={faDumbbell} />
+    </div>
+    );
+};
+
+export default Header;
